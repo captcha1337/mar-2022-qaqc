cd003678766fb8005782474c6cba4cc5f2785af1
captcha1337 <nissans2000@gmail.com>
2022-04-17 22:08:59 +0300

    ...

---
+++ 2nd module "JS"/lesson5/homework/main.js
@@ -0,0 +1,175 @@
+let hello = 'hello';
+console.log(hello);
+// alert(hello);
+document.write(hello);
+
+let owu = 'owu';
+console.log(owu);
+// alert(owu);
+document.write(owu);
+
+let com = 'com';
+console.log(com);
+// alert(com);
+document.write(com);
+
+let ua = 'ua';
+console.log(ua);
+// alert(ua);
+document.write(ua);
+
+let one = 1;
+console.log(one);
+// alert(one);
+document.write(one);
+
+let ten = 10;
+console.log(ten);
+// alert(ten);
+document.write(ten);
+
+let m999 = -999;
+console.log(m999);
+// alert(m999);
+document.write(m999);
+
+let ott = 123;
+console.log(ott);
+// alert(ott);
+document.write(ott);
+
+let numOfPi = 3.14;
+console.log(numOfPi);
+// alert(numOfPi);
+document.write(numOfPi);
+
+let twopsev = 2.7;
+console.log(twopsev);
+// alert(twopsev);
+document.write(twopsev);
+
+let sixteen = 16;
+console.log(sixteen);
+// alert(sixteen);
+document.write(sixteen);
+
+let booltrue = 1 < 2;
+console.log(booltrue);
+// alert(booltrue);
+document.write(booltrue);
+
+let boolfalse = 1 > 2;
+console.log(boolfalse);
+// alert(boolfalse);
+document.write(boolfalse);
+
+///////////////////////////////////////////////////////////////////////////////
+
+let book1 = {
+    name: 'The Great Gatsby',
+    pages: 193,
+    genre: 'novel, fiction, tragedy'
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+let book2 = {
+    name: 'Blackout',
+    pages: 256,
+    genre: 'fiction',
+    authors: [
+        {name: 'Angie Thomas'},
+        {name: 'Nicola Yoon' },
+        {name: 'Tiffany D. Jackson'},
+        {name: 'Ashley Woodfolk'},
+        {name: 'Dhonielle Clayton'},
+        {name: 'Nic Stone'}
+    ]
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+let firstName = 'Ihor';
+let middleName = 'Mykhailovych';
+let lastName = 'Kurbanov';
+
+let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person);
+
+///////////////////////////////////////////////////////////////////////////////
+
+// let firstname = prompt('Enter your first name');
+// let middlename = prompt('Enter your middle name');
+// let age = prompt('Enter your age');
+// console.log(`${firstname} ${middlename} ${age}`);
+
+///////////////////////////////////////////////////////////////////////////////
+
+let a = 100;
+console.log(typeof a)
+
+let b = '100';
+console.log(typeof b)
+
+let c = true;
+console.log(typeof c)
+
+///////////////////////////////////////////////////////////////////////////////
+
+let books = [
+    {
+        name: 'Good Omens',
+        pages: 288,
+        genre: 'horror, fantasy, comedy',
+        authors: [
+            {name: 'Neil Gaiman'},
+            {name: 'Terry Pratchett'}
+        ]
+    },
+    {
+        name: 'The Cinderella Murder',
+        pages: 400,
+        genre: 'fiction, thrillersm suspense',
+        authors: [
+            {name: 'Mary Higgins Clark'},
+            {name: 'Alafair Burke'}
+        ]
+    },
+    {
+        name: 'The Talisman',
+        pages: 656,
+        genre: 'dark fantasy',
+        authors: [
+            {name: 'Stephen King'},
+            {name: 'Peter Straub'}
+        ]
+    },
+    {
+        name: 'Beyond the End of the World',
+        pages: 464,
+        genre: 'young adult fiction, fantasy, epic',
+        authors: [
+            {name: 'Amie Kaufman'},
+            {name: 'Meagan Spooner'}
+        ]
+    },
+    {
+        name: 'Honor Among Thieves',
+        pages: 480,
+        genre: 'science fiction, young adult fiction',
+        authors: [
+            {name: 'Ann Aguirre'},
+            {name: 'Rachel Caine'}
+        ]
+    }
+]
+
+console.log(books[0]);
+console.log(books[1]);
+console.log(books[2]);
+console.log(books[3]);
+console.log(books[4]);
+
+///////////////////////////////////////////////////////////////////////////////
+
+
