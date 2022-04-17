9f54c30fe5b6c7ae2e6e2e0f3d16597ffcb0b64d
captcha1337 <nissans2000@gmail.com>
2022-04-17 22:50:34 +0300

    ...

---
+++ 2nd module "JS"/lesson5/homework/error/main.js
@@ -0,0 +1,18 @@
+// Ваше завдання, зробити так, щоб файл відкривався без помилок .
+
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+let y = 200;
+let x = 'constant value';
+{
+    console.log(a);
+    console.log(d);
+    let x = 'new value';
+    console.log(x)
+
+}
+let result = 100 + y;
+
+console.log(result);
