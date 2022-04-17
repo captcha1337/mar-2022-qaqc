42755c4966aa5572b2075260eba63e07e4ae3eb9
captcha1337 <nissans2000@gmail.com>
2022-04-17 20:35:06 +0300

    ...

---
+++ lesson5 JS/demo1/main.js
@@ -0,0 +1,153 @@
+// let fruits = [
+//     'apple',
+//     'orange',
+//     'banana',
+//     'pinapple',
+//     'milk',
+//
+//
+// ]
+//
+// for (const xFruit of fruits) {
+//     document.write(`<div>${xFruit}</div>`)
+// }
+
+// alert('hello otken!')
+//
+// document.write();
+
+// console.log('LOLKEK')
+//
+// // let a;
+// let a = 'hello';
+// console.log( a );
+//
+// a = 'okten';
+// console.log( a );
+
+// let a = 'string';
+// let b = 100;
+// let c = 100.123;
+
+// {
+//     var a = 'sklhsdakfjs';
+//     let b = 'alasdlkjgasd';
+//
+// }
+//
+// console.log(a);
+// console.log(b);
+// let a = 'fasasdasd';
+// const b = "asdlasda";
+
+// > < >= <= == === != !==
+// let bool1 = 5 > 6;
+// console.log(bool1);
+// let bool2 = 100 < 200;
+// console.log(bool2);
+// let bool3 = 5 === '5';
+// console.log(bool3);
+
+// let f1 = 'apple';
+// let fruits = [ f1, 'orange', 'strawberry' ];
+// console.log(fruits)
+
+// let user1 = {
+//     name: 'vasya',
+//     age: 31,
+//     status: true
+// };
+//
+//
+// let user2 = {
+//     name: 'petya',
+//     age: 34,
+//     status: true
+// };
+//
+// console.log(user1.name)
+// console.log(user2.status)
+//
+// let avgAge = (user1.age + user2.age) / 2;
+// console.log(avgAge)
+//
+// let field = prompt( 'enter var');
+// console.log(user2[field]);
+
+// let user = {
+//     name: 'vasya',
+//     age: 33,
+//     wife: {
+//         name: 'olya',
+//         age: 19,
+//         children: [
+//             {name: 'taras', age: 2},
+//             {name: 'andrii', age: 3}
+//         ]
+//
+//     }
+// }
+//
+//
+// console.log(user);
+// console.log(user.wife.children[1]);
+
+// let matrix = [
+//     'a',
+//     'b',
+//     [
+//         'aa',
+//         'bb',
+//     ],
+//     'c'
+// ]
+//
+// console.log(matrix.length);
+// console.log(matrix[2][1])
+//
+//
+
+// let customer = {
+//     name: 'Serhii',
+//     surname: 'Zhuravlov',
+//     age: 34,
+//     couses: [
+//         'JCX',
+//         'qa'
+//     ]
+// }
+
+// console.log((10 + 20) / 2);
+// console.log(10 % 3);
+
+// let num = 100500;
+// let w1 = 'hello';
+// let w2 = 'okten';
+// // console.log(w1 + ' ' + w2 + ' ' + num);
+// // template string
+//
+// console.log(`${w1} ${w2} ${num}`)
+//
+//
+// let name = 'vasya';
+// console.log(`hello my dear ${name}, how are you?`);
+
+// console.log(100 + 200 + '!');
+// console.log('!' + 100 + 200);
+
+// console.log(typeof 100);
+// console.log(typeof "100");
+// console.log(+'100');
+// console.log(+'txt');
+// console.log(!!'') // false by default
+
+
+console.log(!!'0')
+
+
+
+
+
+
+
+
