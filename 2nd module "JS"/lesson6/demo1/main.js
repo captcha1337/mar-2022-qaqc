83574477a831b2091341df770185fbf41b3ba704
captcha1337 <nissans2000@gmail.com>
2022-04-25 00:22:23 +0300

    ...

---
+++ 2nd module "JS"/lesson6/demo1/main.js
@@ -0,0 +1,99 @@
+//
+// let answer = confirm("r u older than 18?");
+//
+// if (answer) {
+//     document.write("<div>Welcome to our website</div>");
+//     console.log("welcome");
+// } else {
+//     document.write("<div>Get the hell outta here </div>");
+//     console.log("get out");
+// }
+//
+
+
+// let trafficlight = )
+
+// debugger;
+
+// let trafficlight = prompt("which color the light is?");
+//
+// let isRoadClear = confirm("road is clear?");
+//
+// if (trafficlight === "green" || isRoadClear) {
+//     console.log("u can cross the road!");
+// } else if (trafficlight === "yellow") {
+//     console.log("wait");
+// } else if (trafficlight === "red") {
+//     console.log("stop!");
+// }
+// // } else {
+//     console.log("LOL, r u sure?");
+    // console.log("page will reload after 5sec, try again");
+    // setTimeout(function (){
+    //     location.reload();
+    // }, 5000);
+
+
+
+//
+// switch (prompt("light?")){
+//     case "green":
+//         console.log("u can go");
+//         break;
+//     case "yellow":
+//         console.log("wait");
+//         break;
+//     case "red":
+//         console.log("stop");
+//         break;
+//     default:
+//         console.log("????");
+// }
+
+
+
+// let result = confirm("do u want to init variable?");
+// let x;
+// if (result) {
+//     x = 10;
+// }
+// console.log(x);
+
+// let x = confirm("do you want to init variable") ?
+//     10 : undefined;
+//
+// console.log(x);
+
+
+// falsely
+
+console.log(!!'');
+console.log(0);
+console.log(undefined);
+console.log(null);
+console.log(NaN);
+
+// if () {
+//     console.log("truly");
+// } else {
+//     console.log("falsely");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2nd module "JS"/lesson6/homework/main.js
@@ -0,0 +1,91 @@
+// let x = prompt("enter any integer");
+//
+// if (x != 0) {
+//     console.log(" virno");
+// } else {
+//     console.log("ne virno");
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let time = prompt("number 0-59");
+//
+// if (time > 0 && time <= 15) {
+//     console.log("1st quarter");
+// } else if (time > 15 && time <= 30) {
+//     console.log("2nd quarter");
+// } else if (time > 30 && time <= 45) {
+//     console.log("3rd quarter");
+// } else if (time > 45 && time <= 60) {
+//     console.log("4th quarter");
+// } else {
+//     console.log("no number entered");
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// let day = prompt("day number 1-31");
+//
+// if (day > 0 && day <= 10) {
+//     console.log("1st decade");
+// } else if (day > 10 && day <= 20) {
+//     console.log("2nd decade");
+// } else if (day > 20 && day <= 31) {
+//     console.log("3rd decade");
+// } else {
+//     console.log("no day number entered")
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// switch (prompt("enter a day of week")) {
+//     case "monday":
+//         console.log(" MONDAY TODO LIST ");
+//         break;
+//     case "tuesday":
+//         console.log(" TUESDAY TODO LIST ");
+//         break;
+//     case "wednesday":
+//         console.log(" WEDNESDAY TODO LIST ");
+//         break;
+//     case "thursday":
+//         console.log(" THURSDAY TODO LIST ");
+//         break;
+//     case "friday":
+//         console.log(" FRIDAY TODO LIST ");
+//         break;
+//     case "saturday":
+//         console.log(" SATURDAY TODO LIST ");
+//         break;
+//     case "sunday":
+//         console.log(" SUNDAY TODO LIST ");
+//         break;
+//     default:
+//         console.log("no day of week entered")
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let a = prompt("enter 1st number");
+// let b = prompt("enter 2nd number");
+//
+// if (a > b) {
+//     console.log(a);
+// } else if (a < b) {
+//     console.log(b);
+// } else if (a === b) {
+//     console.log("numbers are equal");
+// }
+
+
+
+
+
+
+
+
+
+
