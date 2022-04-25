73d7829031389229b3d6e9adbfa3bee0bfe5fa63
captcha1337 <nissans2000@gmail.com>
2022-04-25 23:49:47 +0300

    ...

---
+++ 2nd module "JS"/lesson3 (7)/demo1/main.js
@@ -0,0 +1,33 @@
+// let x = 0; //0
+// x++; //1
+// x++; //2 increment
+// x--; //1 decrement
+//
+// console.log(x);
+
+// let x = 0;
+// // let y = x++; // result be 1 0 cuz increment type x++
+// let z = ++x; // result be 1 1 cuz increment type ++z
+// console.log(x, z);
+
+
+// document.write("<ol>")
+//
+// for ( let i = 0; i < 10; i++) {
+//     document.write(`<li>list item ${i} </li>`)
+// }
+//
+// document.write("</ol>")
+
+
+let i = 0;
+while (i<10) {
+    document.write(`<li>while item ${i}</li>`);
+    i++;
+}
+
+
+
+
+
+
---
+++ 2nd module "JS"/lesson4 (8)/demo1/main.js
@@ -0,0 +1,35 @@
+// function func() {
+//     document.write(<div>1</div>);
+//     document.write(<div>2</div>);
+//     document.write(<div>3</div>);
+//     document.write(<div>4</div>);
+// }
+//
+//
+//
+// func();
+// func();
+// func();
+// func();
+// func();
+//
+//
+// let a1 = 10;
+// let b1 = 15;
+// let result = (a1 + b1) * 2;
+//
+// console.log(result);
+
+// function divCreator (text) {
+//
+//     document.write(`<div>${text}</div>`);
+// }
+//
+//
+//
+// divCreator("hello");
+// divCreator("okten");
+// divCreator("!!!");
+
+
+
