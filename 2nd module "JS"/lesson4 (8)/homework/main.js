9d9c7ac8231b3d558205a6e839142b219b0f24e8
captcha1337 <nissans2000@gmail.com>
2022-04-27 00:05:13 +0300

    ...

---
+++ 2nd module "JS"/lesson4 (8)/homework/main.js
@@ -0,0 +1,98 @@
+// function rectangleArea(a,b) {
+//     let result = a * b;
+//     return result;
+// }
+//
+// console.log(rectangleArea(+prompt("side a"), +prompt("side b")));
+
+////////////////////////////////////////////////////////////////////
+
+// let pNum = 3.14;
+//
+// function cirleArea(r) {
+//     let result = pNum * (r * r);
+//     return result;
+// }
+//
+// console.log(cirleArea(+prompt("circle radius")));
+
+
+//////////////////////////////////////////////////////////////////////
+
+// let pNum = 3.14;
+
+// function cylinderArea(h, r) {
+//     let result = 2 * pNum * r * (h + r);
+//     return result;
+// }
+//
+// console.log(cylinderArea(+prompt("cylinder height"), +prompt("cylinder radius")));
+
+
+
+//////////////////////////////////////////////////////////////////////
+
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ]
+//
+//
+// function userss(array) {
+//     for (const user of array) {
+//         console.log(user);
+//     }
+//
+// }
+//
+// userss(users);
+
+////////////////////////////////////////////////////////
+
+// function p2Creator(array) {
+//     document.write(`<h2>${array}</h2>`)
+// }
+//
+// p2Creator("LOL KEK")
+//
+// p2Creator("LOL KEK")
+//
+// p2Creator("LOL KEK")
+//
+
+
+///////////////////////////////////////////////////////
+
+// function unordlistCreator(array) {
+//     document.write(`<ul> <li>${array}</li> <li>${array}</li>  <li>${array}</li></ul>`);
+// }
+//
+//
+// unordlistCreator("LOL KEK CHEBUREK");
+
+//////////////////////////////////////////////////////////
+
+
+function unordlistCreator(array) {
+    document.write(`<ul><li>${array}</li></ul>`);
+}
+
+
+let i = unordlistCreator("LOL KEK CHEBUREK");
+
+
+for (i = 0; i < array; i++) {
+    const arrayElement = array;
+
+}
+
+
